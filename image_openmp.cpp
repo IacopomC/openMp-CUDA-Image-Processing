509fa5e9cd09b4cba753d6d28a7e7c2a73953a63
U-EMPTRECIEREC006\ixrplatform <iacopomcatalano@gmail.com>
2021-04-21 07:06:02 +0200

    fix laplacian

--- image_openmp.cpp
+++ image_openmp.cpp
@@ -46,35 +46,93 @@ void laplacianConvOpenmp(cv::Mat_<cv::Vec3b>& src, cv::Mat_<cv::Vec3b>& dst)
 
     float laplacian[9] = { -1.0, -1.0, -1.0, -1.0 , 8.0, -1.0, -1.0, -1.0, -1.0 };
 
+    const int laplace_mat[3][3] = {
+    {-1, -1, -1},
+    {-1,  8, -1},
+    {-1, -1, -1}
+    };
+
     #pragma omp parallel for
     for (int i = k; i < src.rows - k; i++)
     {
         for (int j = k; j < src.cols - k; j++)
         {
             int counter = 0;
+            //float tmp[3] = { 0.0, 0.0, 0.0, };
+            cv::Vec3f tmp = cv::Vec3f(0, 0, 0);
+            for (int u = i - k; u <= i + k; u++)
+            {
+                for (int v = j - k; v <= j + k; v++)
+                {
+                    tmp += (cv::Vec3f)src.at<cv::Vec3b>(u, v) * laplace_mat[u - i + k][v - j + k];
+                }
+            }
+
+            dst.at<cv::Vec3b>(i - k, j - k) = tmp;
+        }
+    }
+}
+
+void gaussianSepOpenmp(cv::Mat_<cv::Vec3b>& src, cv::Mat_<cv::Vec3b>& dst, cv::Mat_<cv::Vec3b>& tmp_img, int kernelSize, int sigma)
+{
+
+    int k = (kernelSize - 1) / 2;
+
+    dst.create(src.rows - 2 * k, src.cols - 2 * k);
+    dst = cv::Vec3b(0, 0, 0);
+
+    tmp_img.create(src.rows - 2 * k, src.cols - 2 * k);
+    tmp_img = cv::Vec3b(0, 0, 0);
+
+    #pragma omp parallel for
+    for (int i = k; i < src.rows - k; i++)
+    {
+        for (int j = k; j < src.cols - k; j++)
+        {
             float tmp[3] = { 0.0, 0.0, 0.0, };
+            float kernelSum[3] = { 0.0, 0.0, 0.0, };
             for (int u = i - k; u <= i + k; u++)
             {
                 for (int v = j - k; v <= j + k; v++)
                 {
                     for (int c = 0; c < 3; c++)
                     {
-                        tmp[c] += (float)src(u, v)[c] * laplacian[counter];
+                        tmp[c] += (float)src(u, v)[c] * exp(-((u - i) * (u - i) + (v - j) * (v - j)) / (2.0 * sigma * sigma));
+                        kernelSum[c] += exp(-((u - i) * (u - i) + (v - j) * (v - j)) / (2.0 * sigma * sigma));
                     }
-                    counter++;
                 }
             }
 
             for (int c = 0; c < 3; c++)
             {
-                tmp[c] /= 255.0;
-                tmp[c] = pow(tmp[c], 1.0 / 0.2);
-                tmp[c] *= 255.0;
-                dst(i - k, j - k)[c] = (unsigned char)tmp[c];
+                dst(i - k, j - k)[c] = (unsigned char)(tmp[c] / kernelSum[c]);
 
             }
         }
     }
+    /*
+    #pragma omp parallel for
+    for (int i = k; i < src.rows - k; i++) 
+    {
+        for (int j = k; j < src.cols - k; j++)
+        {
+            float tmp[3] = { 0.0, 0.0, 0.0, };
+            float kernelSum[3] = { 0.0, 0.0, 0.0, };
+            for (int u = i - k; u <= i + k; u++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    tmp[c] += (float)tmp_img(u, j)[c] * exp(-((u - i) * (u - i) + j * j) / (2.0 * sigma * sigma));
+                    kernelSum[c] += exp(-((u - i) * (u - i) + j * j) / (2.0 * sigma * sigma));
+                }
+            }
+            for (int c = 0; c < 3; c++)
+            {
+                dst(i - k, j - k)[c] = (unsigned char)(tmp[c] / kernelSum[c]);
+
+            }
+        }
+    }*/
 }
 
 //----------------------CIE Lch----------------------
