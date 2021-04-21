c3369b6897fb23a8727631d084cd6eceb6a0bec0
U-EMPTRECIEREC006\ixrplatform <iacopomcatalano@gmail.com>
2021-04-21 09:08:31 +0200

    fix gaussian cuda convolution

--- image.cpp
+++ image.cpp
@@ -7,7 +7,9 @@ using namespace std;
 
 void startCUDA(cv::cuda::GpuMat& src, cv::cuda::GpuMat& dst, int dimX, int dimY);
 
-void gaussianCUDA(cv::cuda::GpuMat& src, cv::cuda::GpuMat& dst, int dimX, int dimY);
+void gaussianConvCUDA(cv::cuda::GpuMat& src, cv::cuda::GpuMat& dst, int dimX, int dimY, cv::cuda::GpuMat& d_kernelGaussConv, int kernelSize, int sigma);
+
+void laplacianCUDA(cv::cuda::GpuMat& src, cv::cuda::GpuMat& dst, int dimX, int dimY);
 
 void imageCombCUDA(cv::cuda::GpuMat& src, cv::cuda::GpuMat& dst, cv::cuda::GpuMat& src2, int dimX, int dimY, int imageComb, float offSet, float scaleFactor);
 
@@ -15,17 +17,45 @@ void gaussianConvOpenmp(cv::Mat_<cv::Vec3b>& src, cv::Mat_<cv::Vec3b>& dst, int 
 
 void laplacianConvOpenmp(cv::Mat_<cv::Vec3b>& src, cv::Mat_<cv::Vec3b>& dst);
 
+void gaussianSepOpenmp(cv::Mat_<cv::Vec3b>& src, cv::Mat_<cv::Vec3b>& dst, cv::Mat_<cv::Vec3b>& tmp_img, int kernelSize, int sigma);
+
 void colorTransfOpenmp(cv::Mat_<cv::Vec3b>& src, cv::Mat_<cv::Vec3b>& dst, float angle);
 
 void imageCombOpenmp(cv::Mat_<cv::Vec3b>& src, cv::Mat_<cv::Vec3b>& dst, cv::Mat_<cv::Vec3b>& src2, int imageComb, float offSet, float scaleFactor);
 
+cv::Mat_<float> generateGaussianKernel(int kernelSize, int sigma)
+{
+    float PI = 3.14159265358979323846;
+    float constant = 1.0 / (2.0 * PI * pow(sigma, 2));
+    int radius = (kernelSize - 1.0) / 2.0;
+    cv::Mat_<float> h_kernel(kernelSize, kernelSize);
+
+    float sum = 0.0;
+    for (int i = -radius; i < radius + 1; i++) {
+        for (int j = -radius; j < radius + 1; j++)
+        {
+            h_kernel[i + radius][j + radius] = constant * (exp(-(pow(i, 2) + pow(j, 2)) / (2 * pow(sigma, 2))));
+            sum += h_kernel(i + radius, j + radius);
+        }
+
+    }
+
+    for (int i = 0; i < kernelSize; ++i)
+        for (int j = 0; j < kernelSize; ++j)
+            h_kernel(i, j) /= sum;
+
+    return h_kernel;
+}
+
 int main(int argc, char** argv)
 {
-    bool cuda = false;
-    bool kernel = false;
+    bool cuda = true;
+    bool kernel = true;
+
+     // ========== OPENMP ========== //
 
     // gaussian parameters
-    int kernelSize = 9;
+    int kernelSize = 1; // for laplacian, set equal to 1
     int sigma = 30;
 
     // color transform parameters
@@ -36,23 +66,32 @@ int main(int argc, char** argv)
     float offSet = 0.5;
     float scaleFactor = 0.5;
 
+    // =========== CUDA =========== //
+
+    // CUDA gaussian convolution param
+    const int kernelSizeGaussC = 5;
+    int sigmaGaussC = 11;
+
     cv::namedWindow("Original Image", cv::WINDOW_OPENGL | cv::WINDOW_AUTOSIZE);
     cv::namedWindow("Processed Image", cv::WINDOW_OPENGL | cv::WINDOW_AUTOSIZE);
 
     cv::Mat_<cv::Vec3b> h_img = cv::imread(argv[1]);
     cv::Mat_<cv::Vec3b> h_img2 = cv::imread(argv[2]);
-    cv::cuda::GpuMat d_img, d_img2, d_result;
+    cv::Mat_<cv::Vec3b> tmp_img;
+    cv::cuda::GpuMat d_img, d_img2, d_result, d_kernel;
     cv::Mat_<cv::Vec3b> h_result;
+    cv::Mat_<float> h_kernel;
 
-    int border = (int)(kernelSize - 1) / 2;
+    d_img2.upload(h_img2);
+    d_result.upload(h_img);
+
+    int border = (int)(kernelSizeGaussC - 1) / 2;
 
     if (kernel) {
         cv::copyMakeBorder(h_img, h_img, border, border, border, border, cv::BORDER_REPLICATE);
     }
 
     d_img.upload(h_img);
-    d_img2.upload(h_img2);
-    d_result.upload(h_img);
 
     cv::imshow("Original Image", d_img);
 
@@ -77,9 +116,20 @@ int main(int argc, char** argv)
             cout << dim << ":" << diff.count() / iter << endl;
             cout << dim << ":" << iter / diff.count() << endl;
         }*/
+        
+        // ======== GAUSSIAN ======== //
+        h_kernel = generateGaussianKernel(kernelSizeGaussC, sigmaGaussC);
+        d_kernel.upload(h_kernel);
+        gaussianConvCUDA(d_img, d_result, 32, 32, d_kernel, kernelSizeGaussC, sigmaGaussC);
 
-        // gaussianCUDA(d_img, d_result, 32, 32);
-        imageCombCUDA(d_img, d_result, d_img2, 32, 32, imageComb, offSet, scaleFactor);
+        // ======== IMAGE COMBINATION ======== //
+        // imageCombCUDA(d_img, d_result, d_img2, 32, 32, imageComb, offSet, scaleFactor);
+         
+        // ======== LAPLACIAN ======== //
+        //laplacianCUDA(d_img, d_result, 32, 32);
+ 
+        /*d_result.download(h_result);
+        std::cout << h_result;*/
 
         cv::imshow("Processed Image", d_result);
     }
@@ -102,10 +152,20 @@ int main(int argc, char** argv)
         cout << "Time per iteration" << diff.count() / iter << endl;
         cout << "Iterations per second" << iter / diff.count() << endl;*/
 
-        // gaussianConvOpenmp(h_img, h_result, kernelSize, sigma);
-        // laplacianConvOpenmp(h_img, h_result);
+        // ======== GAUSSIAN ======== //
+        gaussianConvOpenmp(h_img, h_result, kernelSize, sigma);
+         
+        // ======== LAPLACIAN ======== //
+        //laplacianConvOpenmp(h_img, h_result);
+        
+        // ======== COLOR TRANSFORM ======== //
         // colorTransfOpenmp(h_img, h_result, angle);
-        imageCombOpenmp(h_img, h_result, h_img2, imageComb, offSet, scaleFactor);
+        
+        // ======== IMAGE COMBINATION ======== //
+        // imageCombOpenmp(h_img, h_result, h_img2, imageComb, offSet, scaleFactor);
+
+        // ======== GAUSSIAN SEPARABLE ======== //
+        //gaussianSepOpenmp(h_img, h_result, tmp_img, kernelSize, sigma);
 
         cv::imshow("Processed Image", h_result);
     }
--- image.cu
+++ image.cu
@@ -26,24 +26,67 @@ __global__ void process(const cv::cuda::PtrStep<uchar3> src, cv::cuda::PtrStep<u
     }
 }
 
-__global__ void gaussian(const cv::cuda::PtrStep<uchar3> src, cv::cuda::PtrStep<uchar3> dst, int rows, int cols)
+__global__ void gaussianConv(const cv::cuda::PtrStep<uchar3> src, cv::cuda::PtrStep<uchar3> dst, int rows, int cols, cv::cuda::PtrStep<float> d_kernelGaussConv, int kernelSize, int sigma)
 {
 
     const int dst_x = blockDim.x * blockIdx.x + threadIdx.x;
     const int dst_y = blockDim.y * blockIdx.y + threadIdx.y;
 
-    uchar3 full = make_uchar3(255, 255, 255);
+    const float k = (kernelSize - 1.0) / 2.0;
 
-
-    if (dst_x < cols && dst_y < rows)
+    if (dst_x < cols - k && dst_y < rows - k &&
+        dst_x > k && dst_y > k)
     {
-        uchar3 val = src(dst_y, dst_x);
+        float tmp[3] = { 0.0, 0.0, 0.0, };
 
-        dst(dst_y, dst_x).x = full.x - val.x;
-        dst(dst_y, dst_x).y = full.y - val.y;
-        dst(dst_y, dst_x).z = full.z - val.z;
+        for (int u = dst_x - k; u <= dst_x + k; u++)
+        {
+            for (int v = dst_y - k; v <= dst_y + k; v++)
+            {
+                tmp[0] += (float)src(v, u).x * d_kernelGaussConv(v - dst_y + k, u - dst_x + k);
+            
+                tmp[1] += (float)src(v, u).y * d_kernelGaussConv(v - dst_y + k, u - dst_x + k);
+
+                tmp[2] += (float)src(v, u).z * d_kernelGaussConv(v - dst_y + k, u - dst_x + k);
+            }
+        }
+        dst(dst_y, dst_x).x = (unsigned char)(tmp[0]);
+        dst(dst_y, dst_x).y = (unsigned char)(tmp[1]);
+        dst(dst_y, dst_x).z = (unsigned char)(tmp[2]);
+        /*
+        dst(dst_y, dst_x).x = src(dst_y, dst_x).x;
+        dst(dst_y, dst_x).y = src(dst_y, dst_x).y;
+        dst(dst_y, dst_x).z = src(dst_y, dst_x).z;*/
+    }
+
+}
+
+__global__ void laplacianFilter(const cv::cuda::PtrStep<uchar3> src, cv::cuda::PtrStep<uchar3> dst, int rows, int cols)
+{
+    
+    const int dst_x = blockDim.x * blockIdx.x + threadIdx.x;
+    const int dst_y = blockDim.y * blockIdx.y + threadIdx.y;
+    
+    if (dst_x < cols - 1 && dst_y < rows - 1 &&
+        dst_x > 1 && dst_y > 1)
+    {
+        
+        // Sum of pixel values 
+        float sum[3] = { 0.0, 0.0, 0.0 };
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                sum[0] += (float)(src(dst_y + i, dst_x + j).x * -1.0);
+                sum[1] += (float)(src(dst_y + i, dst_x + j).y * -1.0);
+                sum[2] += (float)(src(dst_y + i, dst_x + j).z * -1.0);
+            }
+        }
+        dst(dst_y, dst_x).x = sum[0] + (src(dst_y, dst_x).x * 9.0);
+        dst(dst_y, dst_x).y = sum[1] + (src(dst_y, dst_x).y * 9.0);
+        dst(dst_y, dst_x).z = sum[2] + (src(dst_y, dst_x).z * 9.0);
+
     }
 }
+
 
 __global__ void imageCombination(const cv::cuda::PtrStep<uchar3> src, cv::cuda::PtrStep<uchar3> dst, const cv::cuda::PtrStep<uchar3> src2, int rows, int cols, int imageComb, float offSet, float scaleFactor)
 {
@@ -98,13 +141,23 @@ void startCUDA(cv::cuda::GpuMat& src, cv::cuda::GpuMat& dst, int dimX, int dimY)
 
 }
 
-void gaussianCUDA(cv::cuda::GpuMat& src, cv::cuda::GpuMat& dst, int dimX, int dimY)
+void gaussianConvCUDA(cv::cuda::GpuMat& src, cv::cuda::GpuMat& dst, int dimX, int dimY, cv::cuda::GpuMat& d_kernelGaussConv, int kernelSize, int sigma)
 {
 
     const dim3 block(dimX, dimY);
     const dim3 grid(divUp(dst.cols, block.x), divUp(dst.rows, block.y));
 
-    gaussian << <grid, block >> > (src, dst, dst.rows, dst.cols);
+    gaussianConv << <grid, block >> > (src, dst, dst.rows, dst.cols, d_kernelGaussConv, kernelSize, sigma);
+
+}
+
+void laplacianCUDA(cv::cuda::GpuMat& src, cv::cuda::GpuMat& dst, int dimX, int dimY)
+{
+
+    const dim3 block(dimX, dimY);
+    const dim3 grid(divUp(dst.cols, block.x), divUp(dst.rows, block.y));
+
+    laplacianFilter << <grid, block >> > (src, dst, dst.rows, dst.cols);
 
 }
 
