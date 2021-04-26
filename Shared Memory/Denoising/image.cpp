e1ecb99e41b09f733d542a2226d920d149de2646
U-EMPTRECIEREC006\ixrplatform <iacopomcatalano@gmail.com>
2021-04-26 20:38:55 +0200

    add denoising shared memory

---
+++ Shared Memory/Denoising/image.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <stdlib.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/cudawarping.hpp>
+#include <cuda_runtime.h>
+#include <chrono>  // for high_resolution_clock
+
+#define RADIUS 4
+
+using namespace std;
+
+void denoisingCUDA(cv::cuda::GpuMat& src, cv::cuda::GpuMat& dst, int kernelSize, int percent);
+
+int main(int argc, char** argv)
+{
+    cv::namedWindow("Original Image", cv::WINDOW_OPENGL | cv::WINDOW_AUTOSIZE);
+    cv::namedWindow("Processed Image", cv::WINDOW_OPENGL | cv::WINDOW_AUTOSIZE);
+
+    cv::Mat_<cv::Vec3b> h_img = cv::imread(argv[1]);
+    cv::cuda::GpuMat d_result;
+    cv::cuda::GpuMat d_img;
+    cv::Mat_<cv::Vec3b> input_img;    
+
+    int percent = atof(argv[2]);
+    const int kernelSize = 2 * RADIUS + 1; // change RADIUS in other file accordingly
+
+    int border = RADIUS;
+
+    cv::copyMakeBorder(h_img, input_img, border, border, border, border, cv::BORDER_REPLICATE);
+
+    d_img.upload(input_img);
+    d_result.upload(input_img);
+
+    // ============= BEST DIM = 4. TESTED WITH CODE BELOW ============= //
+    denoisingCUDA(d_img, d_result, kernelSize, percent);
+
+    // crop final image to original size
+    d_result = d_result(cv::Range(border + 1, d_result.rows - border), cv::Range(border + 1, d_result.cols - border)).clone();
+
+    /*
+    for (int dim = 1; dim < 12; dim++)
+    {
+        auto begin = chrono::high_resolution_clock::now();
+        const int iter = 100;
+
+
+        for (int i = 0; i < iter; i++)
+        {
+            colorTransfCUDA(d_img, d_result, dim, dim, angle);
+        }
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> diff = end - begin;
+
+        cout << "Total time dim " << dim << ":" << diff.count() << endl;
+        cout << "Time per iteration dim " << dim << ":" << diff.count() / iter << endl;
+        cout << "Iterations per second dim " << dim << ":" << iter / diff.count() << endl;
+    }
+    */
+
+    cv::imshow("Original Image", h_img);
+    cv::imshow("Processed Image", d_result);
+
+    cv::waitKey();
+    return 0;
+}
---
+++ Shared Memory/Denoising/kernel.cu
@@ -0,0 +1,168 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include <opencv2/opencv.hpp>
+#include <cfloat>
+#include <opencv2/core/cuda/common.hpp>
+#include <opencv2/core/cuda/border_interpolate.hpp>
+#include <opencv2/core/cuda/vec_traits.hpp>
+#include <opencv2/core/cuda/vec_math.hpp>
+#include <cuda_runtime.h>
+#include <chrono>  // for high_resolution_clock
+
+#define BLOCK_SIZE 32
+#define RADIUS 4
+
+__global__ void denoising(const cv::cuda::PtrStep<uchar3> src, cv::cuda::PtrStep<uchar3> dst, int rows, int cols, int kernelSize, int percent)
+{
+    __shared__ uchar3 temp[BLOCK_SIZE + 2 * RADIUS][BLOCK_SIZE + 2 * RADIUS];
+
+    // local indices
+    int lindex_X = threadIdx.x + RADIUS;
+    int lindex_Y = threadIdx.y + RADIUS;
+
+    int dst_x = blockDim.x * blockIdx.x + lindex_X;
+    int dst_y = blockDim.y * blockIdx.y + lindex_Y;
+
+    if (dst_x < cols && dst_y < rows)
+    {
+
+        // Read input elements into shared memory
+        temp[lindex_Y][lindex_X] = src(dst_y, dst_x);
+        if (threadIdx.x < RADIUS) {
+            temp[lindex_Y][lindex_X - RADIUS] = src(dst_y, dst_x - RADIUS);
+            if (dst_x + BLOCK_SIZE < cols)
+                temp[lindex_Y][lindex_X + BLOCK_SIZE] = src(dst_y, dst_x + BLOCK_SIZE);
+        }
+
+        if (threadIdx.y < RADIUS) {
+            temp[lindex_Y - RADIUS][lindex_X] = src(dst_y - RADIUS, dst_x);
+            if (dst_y + BLOCK_SIZE < rows)
+                temp[lindex_Y + BLOCK_SIZE][lindex_X] = src(dst_y + BLOCK_SIZE, dst_x);
+        }
+
+        if (threadIdx.y < RADIUS && threadIdx.x < RADIUS) {
+            temp[lindex_Y - RADIUS][lindex_X - RADIUS] = src(dst_y - RADIUS, dst_x - RADIUS);
+            if (dst_y + BLOCK_SIZE < rows && dst_x + BLOCK_SIZE < cols)
+                temp[lindex_Y + BLOCK_SIZE][lindex_X + BLOCK_SIZE] = src(dst_y + BLOCK_SIZE, dst_x + BLOCK_SIZE);
+            if (dst_x + BLOCK_SIZE < cols)
+                temp[lindex_Y - RADIUS][lindex_X + BLOCK_SIZE] = src(dst_y - RADIUS, dst_x + BLOCK_SIZE);
+            if (dst_y + BLOCK_SIZE < rows)
+                temp[lindex_Y + BLOCK_SIZE][lindex_X - RADIUS] = src(dst_y + BLOCK_SIZE, dst_x - RADIUS);
+        }
+    }
+
+    // Synchronize (ensure all the data is available)
+    __syncthreads();
+
+    const int k = (kernelSize - 1.0) / 2.0;
+
+    if (dst_x < cols - k && dst_y < rows - k &&
+        dst_x > k && dst_y > k)
+    {
+        float rNeighbors[144];
+        float gNeighbors[144];
+        float bNeighbors[144];
+
+        float rValue = 0.0;
+        float gValue = 0.0;
+        float bValue = 0.0;
+
+        int counter = 0;
+
+        for (int i = -RADIUS; i <= RADIUS; i++)
+        {
+            for (int j = -RADIUS; j <= RADIUS; j++)
+            {
+
+                uchar3 cur = temp[lindex_Y + i][lindex_X + j];
+
+                rNeighbors[counter] = (float)cur.z;
+                gNeighbors[counter] = (float)cur.y;
+                bNeighbors[counter] = (float)cur.x;
+                counter++;
+            }
+        }
+
+        int key, j;
+        for (int i = 1; i < k * k; i++)
+        {
+            key = rNeighbors[i];
+            j = i - 1;
+
+            while (j >= 0 && rNeighbors[j] > key)
+            {
+                rNeighbors[j + 1] = rNeighbors[j];
+                j = j - 1;
+            }
+            rNeighbors[j + 1] = key;
+        }
+
+        for (int i = 1; i < k * k; i++)
+        {
+            key = gNeighbors[i];
+            j = i - 1;
+
+            while (j >= 0 && gNeighbors[j] > key)
+            {
+                gNeighbors[j + 1] = gNeighbors[j];
+                j = j - 1;
+            }
+            gNeighbors[j + 1] = key;
+        }
+
+        for (int i = 1; i < k * k; i++)
+        {
+            key = bNeighbors[i];
+            j = i - 1;
+
+            while (j >= 0 && bNeighbors[j] > key)
+            {
+                bNeighbors[j + 1] = bNeighbors[j];
+                j = j - 1;
+            }
+            bNeighbors[j + 1] = key;
+        }
+
+        int medianIndx = 0;
+        if (kernelSize % 2 == 0) {
+            medianIndx = kernelSize * kernelSize / 2;
+        }
+        else {
+            medianIndx = (kernelSize * kernelSize - 1) / 2;
+        }
+
+        int numEl = (kernelSize * kernelSize * int(percent) / 100) / 2;
+
+        for (int w = (medianIndx - numEl); w <= (medianIndx + numEl); w++) {
+            rValue += rNeighbors[w];
+            gValue += gNeighbors[w];
+            bValue += bNeighbors[w];
+        }
+
+        if (numEl >= 1) {
+            rValue /= float(2 * numEl + 1);
+            gValue /= float(2 * numEl + 1);
+            bValue /= float(2 * numEl + 1);
+        }
+
+        dst(dst_y, dst_x).z = (unsigned char)(rValue);
+        dst(dst_y, dst_x).y = (unsigned char)(gValue);
+        dst(dst_y, dst_x).x = (unsigned char)(bValue);
+
+    }
+}
+
+int divUp(int a, int b)
+{
+    return ((a % b) != 0) ? (a / b + 1) : (a / b);
+}
+
+void denoisingCUDA(cv::cuda::GpuMat& src, cv::cuda::GpuMat& dst, int kernelSize, int percent)
+{
+
+    const dim3 block(BLOCK_SIZE, BLOCK_SIZE);
+    const dim3 grid(divUp(dst.cols, block.x), divUp(dst.rows, block.y));
+
+    denoising << <grid, block >> > (src, dst, dst.rows, dst.cols, kernelSize, percent);
+
+}
